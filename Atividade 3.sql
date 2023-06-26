9a886df9a072caa0c505abb936072361f76fb77d
Kamylla Morais <kamyllamoraisviana@hotmail.com>
2023-06-26 20:11:18 -0300

    Atualização atividade 3

--- Atividade 3.sql
+++ Atividade 3.sql
@@ -1,16 +1,17 @@
 
-#1)) Para criar um banco de dados utilizar o comando CREATE DATABASE + nome do banco, depois utilizar o comando USE + nome do banco de dados. 
-# Para criar uma tabela, utilizar o comando CREATE TABLE + nome da tabela (. 
-# Incluir os dados com o tipo e quantidade de caracteres da tabela, exemplo nome VARCHAR (50).
-#para incluir dados na tabela, utilizar o código DESC + nome da tabela (descreve a tabela e quantidade de caracteres que podem ser incluidos 
-# Utilizar o código INSERT INTO + nome da tabela VALUES ( "THÉO", 10 , "61999875001", "RIACHO FUNDO 2, DF, BRASÍLIA ", "theo@hotmail.com") - a vírgula separa a informação que será inclusa 
-# na próxima coluna da tabela. 
-# Para consultar dados da tabela, utilizar o código SELECT * FROM + nome da tabela. 
+/*
+1)) Para criar um banco de dados utilizar o comando CREATE DATABASE + nome do banco, depois utilizar o comando USE + nome do banco de dados. 
+Para criar uma tabela, utilizar o comando CREATE TABLE + nome da tabela (. 
+Incluir os dados com o tipo e quantidade de caracteres da tabela, exemplo nome VARCHAR (50).
+Para incluir dados na tabela, utilizar o código DESC + nome da tabela (descreve a tabela e quantidade de caracteres que podem ser incluidos 
+Utilizar o código INSERT INTO + nome da tabela VALUES ( "THÉO", 10 , "61999875001", "RIACHO FUNDO 2, DF, BRASÍLIA ", "theo@hotmail.com") - a vírgula separa a informação que será inclusa 
+na próxima coluna da tabela. 
+Para consultar dados da tabela, utilizar o código SELECT * FROM + nome da tabela. 
 
-# 2)) - SELECT COUNT(*): Contar os dados selecionados. 
-# UPDATE: atualizar informação da tabela (trocar dados).
-# ALTER TABLE: Alterar o nome da coluna.
-# DROP TABLE : Apaga a tabela.  
+2)) - SELECT COUNT(*): Contar os dados selecionados. 
+UPDATE: atualizar informação da tabela (trocar dados).
+ALTER TABLE: Alterar o nome da coluna.
+DROP TABLE : Apaga a tabela. */  
 
 CREATE DATABASE produto ;
 USE produto ;
